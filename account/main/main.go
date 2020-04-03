c663e4afe37abbcfe794d5b4ba8a71d527e8e174
XB <ssbandjl@163.com>
2020-04-03 16:30:32 +0800

    1

--- account/main/main.go
+++ account/main/main.go
@@ -1,9 +1,11 @@
 package main
+
 import (
-	"go_code/account/utils"
 	"fmt"
+	"go_code/account/utils"
 )
+
 func main() {
 	fmt.Println("面向对象的方式来完成.....")
-	utils.NewMyFamilyAccount().MainMenu()
+	utils.NewMyFamilyAccount().MainMenu() //NewMyFamilyAccount返回了一个结构体指针，该结构体绑定了一个MainMenu方法
 }
