d421f1a625f3d2d623cfb8744f227b511ce56f61
XB <ssbandjl@163.com>
2020-04-07 22:20:55 +0800

    1

--- chapter20/selectsort/main.go
+++ chapter20/selectsort/main.go
@@ -22,12 +22,12 @@ func SelectSort(arr *[80000]int) {
 		maxIndex := j
 		//2. 遍历后面 1---[len(arr) -1] 比较
 		for i := j + 1; i < len(arr); i++ {
-			if max < arr[i] { //找到真正的最大值
+			if max < arr[i] { //找到真正的最大值，先不要交换
 				max = arr[i]
 				maxIndex = i
 			}
 		}
-		//交换
+		//交换，比较的效率高于交换，所以尽量减少交换次数
 		if maxIndex != j {
 			arr[j], arr[maxIndex] = arr[maxIndex], arr[j]
 		}
