0ed1934405ebad3bf571ffcbb080ccd5ed537a3f
XB <ssbandjl@163.com>
2020-04-16 10:13:16 +0800

    1

--- chapter06/timerange/main.go
+++ chapter06/timerange/main.go
@@ -17,7 +17,7 @@ func main() {
 	//"" , 默认值
 	//"用户名,默认为空" 说明
 	flag.StringVar(&start, "s", "2006-01-02 15:04:05", "开始时间")
-	flag.StringVar(&end, "end", "2006-01-02 15:04:05", "结束时间")
+	flag.StringVar(&end, "e", "2006-01-02 15:04:05", "结束时间")
 	//这里有一个非常重要的操作,转换， 必须调用该方法
 	flag.Parse()
 
@@ -26,6 +26,10 @@ func main() {
 
 	//从字符串转为时间戳，第一个参数是格式，第二个是要转换的时间字符串 fmt.Printf(now.Format("2006-01-02 15:04:05"))
 	startS, _ := time.Parse("2006-01-02 15:04:05", start)
-	// entS, _ := (time.Parse("2006-01-02 15:04:05", end)).Unix()
-	fmt.Println(startS.unix())
+	startStamp := startS.Unix()
+	endS, _ := time.Parse("2006-01-02 15:04:05", end)
+	endStamp := endS.Unix()
+	timeRange := endStamp - startStamp
+	timeRangeMin := timeRange / 60
+	fmt.Printf("\nstartS=%v, endS=%v\ntimeRangeMin=%v\n", startStamp, endStamp, timeRangeMin)
 }
