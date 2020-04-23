27012523b20ad434bf389c8e6b75840e3ab61403
XB <ssbandjl@163.com>
2020-04-23 17:41:02 +0800

    1

---
+++ chapter14/json/jsonRawMessage/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+)
+
+type UpLoadSomething struct {
+	Type   string
+	Object interface{}
+}
+
+type File struct {
+	FileName string
+}
+
+type Png struct {
+	Wide  int
+	Hight int
+}
+
+func main() {
+
+	input := `
+    {
+        "type": "File",
+        "object": {
+            "filename": "for test"
+        }
+    }
+    `
+	var object json.RawMessage
+	ss := UpLoadSomething{
+		Object: &object,
+	}
+	if err := json.Unmarshal([]byte(input), &ss); err != nil {
+		panic(err)
+	}
+	switch ss.Type {
+	case "File":
+		var f File
+		if err := json.Unmarshal(object, &f); err != nil {
+			panic(err)
+		}
+		println(f.FileName) //for test
+	case "Png":
+		var p Png
+		if err := json.Unmarshal(object, &p); err != nil {
+			panic(err)
+		}
+		println(p.Wide)
+	}
+}
