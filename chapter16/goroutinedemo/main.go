87b81d2c922dca2c003c5e48db365e3589d7b471
XB <ssbandjl@163.com>
2020-08-05 23:17:34 +0800

    1

--- chapter16/goroutinedemo/main.go
+++ chapter16/goroutinedemo/main.go
@@ -1,8 +1,9 @@
 package main
+
 import (
 	"fmt"
 	"strconv"
-	_"time"
+	_ "time"
 )
 
 // 在主线程(可以理解成进程)中，开启一个goroutine, 该协程每隔1秒输出 "hello,world"
@@ -15,14 +16,16 @@ func test() {
 		fmt.Println("tesst () hello,world " + strconv.Itoa(i))
 		//time.Sleep(time.Second)
 	}
+	fmt.Println("协程程执行完毕")
 }
 
 func main() {
 
 	go test() // 开启了一个协程
 
-	for i := 1; i <= 1000; i++ {
+	for i := 1; i <= 100; i++ {
 		fmt.Println(" main() hello,golang" + strconv.Itoa(i))
 		//time.Sleep(time.Second)
 	}
+	fmt.Println("主线程执行完毕，直接结束")
 }
