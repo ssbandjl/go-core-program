4a049593283f1913895a57c7d9b304ffe652b005
XB <ssbandjl@163.com>
2020-08-10 10:17:40 +0800

    1

---
+++ chapter07/sliceFilter/main.go
@@ -0,0 +1,12 @@
+package main
+
+//利用长度为0的空切片特性，完成切片过滤
+func Filter(s []byte, fn func(x byte) bool) []byte {
+	b := s[:0]
+	for _, x := range s {
+		if !fn(x) {
+			b = append(b, x)
+		}
+	}
+	return b
+}
--- chapter07/trimSpaceFromSlice/main.go
+++ chapter07/trimSpaceFromSlice/main.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
+//利用长度为0的空切片特性，删除字节切片中的空格
 func TrimSpace(s []byte) []byte {
 	b := s[:0]
+	fmt.Println(b)
 	for _, x := range s {
 		if x != ' ' {
 			b = append(b, x)
+			fmt.Println(b)
 		}
 	}
 	return b
