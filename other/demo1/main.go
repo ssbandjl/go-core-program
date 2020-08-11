56b5e3f0c540dbac2501574d1cd2f233818a3092
XB <ssbandjl@163.com>
2020-08-11 17:37:15 +0800

    1

--- other/demo1/main.go
+++ other/demo1/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func sum(s []int, c chan int) {
 	sum := 0
+	// var sum int
 	for _, v := range s {
+		// sum += v
 		sum += v
 	}
 	fmt.Println(sum)
@@ -15,9 +17,10 @@ func main() {
 	s := []int{7, 2, 8, -9, 4, 0}
 
 	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c // receive from c
+	go sum(s[:len(s)/2], c) //0-3  7, 2, 8  =17
+	go sum(s[len(s)/2:], c) //3-5  -9, 4, 0  =-5
+	x, y := <-c, <-c        // receive from c
 
-	fmt.Println(x, y, x+y)
+	// fmt.Println(x, y, x+y)
+	fmt.Printf("x=%v, y=%v, x+y=%v\n", x, y, x+y)
 }
